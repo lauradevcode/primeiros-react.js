7377dfe8107f07c12b19be5ccd6672d813ade96f
lauradevcode <lauracst2013@gmail.com>
2021-02-07 00:51:59 -0300

    att2

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Primeiro() {
+    return (
+        <div>
+            <h1>Primeiro componente criado</h1>
+            <h2>Exemplo de um componente React</h2>
+        </div>
+
+    )
+}
+export default Primeiro
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Primeiro from './components/Primeiro'
+
+ReactDOM.render(
+    <Primeiro></Primeiro>,
+    document.getElementById('root')
+)
